33cc51e0e6e93ac46a5624ab121404e3da78e451
Samuel Munilla <smunilla@redhat.com>
2018-11-30 14:57:26 -0500

    Cleanup util functions

--- elliottlib/util.py
+++ elliottlib/util.py
@@ -27,3 +27,135 @@ def green_print(msg):
 def cprint(msg):
     """Wrapper for click.echo"""
     click.echo(msg)
+
+def exit_unauthorized():
+    """Standard response when an API call returns 'unauthorized' (403)"""
+    red_prefix("Error Unauthorized: ")
+    click.echo("403 - user is authenticated, but unauthorized to perform this action")
+    exit(1)
+
+
+def validate_release_date(ctx, param, value):
+    """Ensures dates are provided in the correct format"""
+    try:
+        release_date = datetime.datetime.strptime(value, YMD)
+        if release_date == default_release_date:
+            # Default date, nothing special to note
+            pass
+        else:
+            # User provided date passed validation, they deserve a
+            # hearty thumbs-up!
+            green_prefix("User provided release date: ")
+            click.echo("{} - Validated".format(release_date.strftime(YMD)))
+        return value
+    except ValueError:
+        raise click.BadParameter('Release date (--date) must be in YYYY-MM-DD format')
+
+def validate_email_address(ctx, param, value):
+    """Ensure that email addresses provided are valid email strings"""
+    # Really just check to match /^[^@]+@[^@]+\.[^@]+$/
+    email_re = re.compile('^[^@ ]+@[^@ ]+\.[^@ ]+$')
+    if not email_re.match(value):
+        raise click.BadParameter(
+            "Invalid email address for {}: {}".format(param, value))
+
+    return value
+
+def release_from_branch(ver):
+    """Parse the release version from the provided 'branch'.
+
+For example, if --group=openshift-3.9 then runtime.group_config.branch
+will have the value rhaos-3.9-rhel-7. When passed to this function the
+return value would be the number 3.9, where in considering '3.9' then
+'3.9' is the RELEASE version.
+
+This behavior is HIGHLY dependent on the format of the input
+argument. Hence, why this function indicates the results are based on
+the 'branch' variable. Arbitrary input will fail. Use of this implies
+you read the docs.
+    """
+    return ver.split('-')[1]
+
+
+def major_from_branch(ver):
+    """Parse the major version from the provided version (or 'branch').
+
+For example, if --group=openshift-3.9 then runtime.group_config.branch
+will have the value rhaos-3.9-rhel-7. When passed to this function the
+return value would be the number 3, where in considering '3.9' then
+'3' is the MAJOR version.
+
+I.e., this gives you the X component if 3.9 => X.Y.
+
+This behavior is HIGHLY dependent on the format of the input
+argument. Hence, why this function indicates the results are based on
+the 'branch' variable. Arbitrary input will fail. Use of this implies
+you read the docs.
+    """
+    return ver.split('-')[1].split('.')[0]
+
+
+def minor_from_branch(ver):
+    """Parse the minor version from the provided version (or 'branch').
+
+For example, if --group=openshift-3.9 then runtime.group_config.branch
+will have the value rhaos-3.9-rhel-7. When passed to this function the
+return value would be the number 9, where in considering '3.9' then
+'9' is the MINOR version.
+
+I.e., this gives you the Y component if 3.9 => X.Y.
+
+This behavior is HIGHLY dependent on the format of the input
+argument. Hence, why this function indicates the results are based on
+the 'branch' variable. Arbitrary input will fail. Use of this implies
+you read the docs.
+    """
+    return ver.split('-')[1].split('.')[1]
+
+
+def pbar_header(msg_prefix='', msg='', seq=[], char='*'):
+    """Generate a progress bar header for a given iterable or
+sequence. The given sequence must have a countable length. A bar of
+`char` characters is printed between square brackets.
+
+    :param string msg_prefix: Header text to print in heavy green text
+    :param string msg: Header text to print in the default char face
+    :param sequence seq: A sequence (iterable) to size the progress
+    bar against
+    :param str char: The character to use when drawing the progress
+    bar
+
+For example:
+
+    pbar_header("Foo: ", "bar", seq=[None, None, None], char='-')
+
+would produce:
+
+    Foo: bar
+    [---]
+
+where 'Foo: ' is printed using green_prefix() and 'bar' is in the
+default console fg color and weight.
+
+TODO: This would make a nice context wrapper.
+
+    """
+    green_prefix(msg_prefix)
+    click.echo(msg)
+    click.echo("[" + (char * len(seq)) + "]")
+
+
+def progress_func(func, char='*'):
+    """Use to wrap functions called in parallel. Prints a character for
+each function call.
+
+    :param lambda-function func: A 'lambda wrapped' function to call
+    after printing a progress character
+    :param str char: The character (or multi-char string, if you
+    really wanted to) to print before calling `func`
+
+    Usage examples:
+      * See advisory:find-builds
+    """
+    click.secho(char, fg='green', nl=False)
+    return func()
