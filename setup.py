0810616fda1cefa7b19457b3a8dd5d5d9e363c2d
Adam Haile <adammhaile@gmail.com>
2018-12-14 14:10:57 -0500

    Adding Travis CI Setup

--- setup.py
+++ setup.py
@@ -6,11 +6,18 @@ from setuptools import setup
 with open('./requirements.txt') as f:
     INSTALL_REQUIRES = f.read().splitlines()
 
+
+def _get_version():
+    from os.path import abspath, dirname, join
+    filename = join(dirname(abspath(__file__)), 'elliottlib', 'VERSION')
+    return open(filename).read().strip()
+
+
 setup(
-    name="elliott",
+    name="rh-elliott",
     author="AOS ART Team",
     author_email="aos-team-art@redhat.com",
-    version="0.1",
+    version=_get_version(),
     description="CLI tool for managing and automating Red Hat software releases",
     url="https://github.com/openshift/elliott",
     license="Red Hat Internal",
