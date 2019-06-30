fd22c72f625c9634145e75fd82d781ca35186bf8
Shiyang Wang <shiywang@redhat.com>
2019-06-30 15:57:52 +0800

    add override_product_version doc string

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,6 +5,7 @@ from elliottlib import util
 
 class UtilTestCase(unittest.TestCase):
     def test_override_product_version(self):
+        """ if user specify -b branch will override product_version of erratatool.yml"""
         ret = util.override_product_version('RHEL-7-OSE-4.1', 'rhaos-4.1-rhel-8-candidate')
         self.assertEqual(ret, 'RHEL-8-OSE-4.1')
 
