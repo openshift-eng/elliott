e1ff5242aebd035b50b20543724693d10e0ceb92
Shiyang Wang <shiywang@redhat.com>
2019-07-01 13:27:18 +0800

    update doc string and add hardcode RHEL8 convert

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,12 +5,14 @@ from elliottlib import util
 
 class UtilTestCase(unittest.TestCase):
     def test_override_product_version(self):
-        """ if user specify -b branch will override product_version of erratatool.yml"""
         ret = util.override_product_version('RHEL-7-OSE-4.1', 'rhaos-4.1-rhel-8-candidate')
-        self.assertEqual(ret, 'RHEL-8-OSE-4.1')
+        self.assertEqual(ret, 'OSE-4.1-RHEL-8')
 
         ret = util.override_product_version('RHEL-7-OSE-4.1', 'rhaos-4.2-rhel-8')
-        self.assertEqual(ret, 'RHEL-8-OSE-4.2')
+        self.assertEqual(ret, 'OSE-4.2-RHEL-8')
+
+        ret = util.override_product_version('RHEL-7-OSE-4.1', 'rhaos-4.2-rhel-7')
+        self.assertEqual(ret, 'RHEL-7-OSE-4.2')
 
         ret = util.override_product_version('RHEL-7-OSE-4.1', 'rhaos-4.1-rhel-7')
         self.assertEqual(ret, 'RHEL-7-OSE-4.1')
