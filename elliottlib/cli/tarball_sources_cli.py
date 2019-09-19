d6db675601e53635de587ef398a95428de063eb8
Yuxiang Zhu <yuxzhu@redhat.com>
2019-09-19 13:39:45 +0000

    ART-485: automate tarball sources for container-first builds
    This change add new command `tarball-sources create`, which allow you to batch create tarball sources for advisories.
    
    By default, it will automatically put generated tarballs into a RCM-friendly directory structure `$product/$product_version/$advisoryid/release/$NVR.tar.gz`. It can also handle the situation that a build (NVR) is attached to multiple advisories.
    
    It works as the following steps:
    
    - Fetch attached builds from specified advisories.
    - Filter out builds of interested component.
    - Look up source repo and commit from Koji/Brew that each build is built from.
    - Generate source tarball for each build.
    - Place generated source tarballs to the output directory with specified directory hierarchy.
    
    This code has been tested on both Python 2.7 and Python 3.7.
    
    Example:
    
    Create tarball sources for component `logging-fluentd-container` shipped in advisories `45606`, `45527`, and `46049`:
    
    ```
    $ elliott tarball-sources create --component logging-fluentd-container --out-dir out/ 45606 45527 46049
    ```
    
    The following tarballs will be placed like:
    
    ```
    RHOSE/
    ├── RHEL-7-OSE-3.11
    │   └── 45606
    │       └── release
    │           └── logging-fluentd-container-v3.11.141-2.tar.gz
    └── RHEL-7-OSE-4.1
        ├── 45527
        │   └── release
        │       └── logging-fluentd-container-v4.1.14-201908291507.tar.gz
        └── 46049
            └── release
                └── logging-fluentd-container-v4.1.15-201909041605.tar.gz
    ```

---
+++ elliottlib/cli/tarball_sources_cli.py
@@ -0,0 +1,175 @@
+from __future__ import unicode_literals, print_function, with_statement
+from future.builtins import str
+import os
+import errno
+import shutil
+import koji
+import errata_tool
+from .. import constants
+import pipes
+from .. import tarball_sources
+import tempfile
+from .. import util
+from .. import logutil
+from typing import List, Set, Dict, Tuple, Text, Optional
+import click
+# https://click.palletsprojects.com/en/7.x/python3/
+click.disable_unicode_literals_warning = True
+
+
+LOGGER = logutil.getLogger(__name__)
+
+
+@click.group("tarball-sources", short_help="Create or publish tarball sources")
+@click.pass_context
+def tarball_sources_cli(ctx):
+    runtime = ctx.obj
+    runtime.initialize(no_group=True)
+
+
+@tarball_sources_cli.command()
+@click.argument("advisories", nargs=-1, type=click.IntRange(1), required=True)
+@click.option("--out-dir", type=click.Path(), default="./", help="Output directory for tarball sources.")
+@click.option("--out-layout", type=click.Choice(["hierarchical", "flat"]),
+              default="hierarchical", show_default=True,
+              help="Layout of output directory.")
+@click.option("--component", "components", multiple=True, help="Koji/Brew component names or build NVRs to filter on. Can be specified multiple times.")
+@click.option("-f", "--force", is_flag=True, help="Force overwrite existing files.")
+@click.pass_context
+def create(ctx, advisories, out_dir, out_layout, components, force):
+    """ Create tarball sources for advisories.
+
+    To create tarball sources for Brew component (package) logging-fluentd-container that was shipped on advisories 45606, 45527, and 46049:
+    $ elliott tarball-sources create --component logging-fluentd-container --out-dir=out/ 45606 45527 46049
+    """
+
+    if not force and os.path.isdir(out_dir) and os.listdir(out_dir):
+        util.red_print(
+            "Output directory {} is not empty.\n\
+Use --force to add new tarball sources to an existing directory.".format(os.path.abspath(out_dir)))
+        exit(1)
+    mkdirs(out_dir)
+
+    working_dir = os.path.join(ctx.obj.working_dir, "tarball-sources")
+    LOGGER.debug("Use working directory {}.".format(
+        os.path.abspath(working_dir)))
+    mkdirs(working_dir)
+
+    # `nvr_dirs` is a dict with brew build NVRs as keys, values are
+    # a set of directories for the generated tarballs,
+    # since a build can be attached to multiple advisories.
+    # For example:
+    # nvr_dirs = {
+    #   "logging-fluentd-container-v3.11.141-2": {
+    #     "RHOSE/RHEL-7-OSE-3.11/45606/release/"
+    #   },
+    #   "logging-fluentd-container-v4.1.14-201908291507": {
+    #     "RHOSE/RHEL-7-OSE-4.1/45527/release/"
+    #   },
+    #   "logging-fluentd-container-v4.1.15-201909041605": {
+    #     "RHOSE/RHEL-7-OSE-4.1/46049/release/"
+    #   }
+    # }
+    nvr_dirs = {}  # type: Dict[str, Set[str]]
+
+    # Getting build NVRs for specified Koji/Brew components from advisories
+    # NOTE This is SLOW. However doing this in parallel doesn't work
+    # due to a race condition existing in the implementation of `errata_tool.Erratum`'s parant class ErrataConnector.
+    for advisory in advisories:
+        click.echo(
+            "Finding builds from advisory {}...".format(advisory))
+        builds = tarball_sources.find_builds_from_advisory(
+            advisory, components)
+        if not builds:
+            util.yellow_print(
+                "No matched builds found from advisory {}. Wrong advisory number?".format(advisory))
+            continue
+        util.green_print(
+            "Found {} matched build(s) from advisory {}".format(len(builds), advisory))
+        for nvr, product, product_version in builds:
+            util.green_print("\t{}\t{}\t{}".format(
+                nvr, product, product_version))
+
+        for nvr, product, product_version in builds:
+            if nvr not in nvr_dirs:
+                nvr_dirs[nvr] = set()
+            if out_layout == "flat":
+                nvr_dirs[nvr].add(out_dir)
+            else:
+                nvr_dirs[nvr].add(os.path.join(
+                    out_dir, product_version, str(advisory), "release"))
+
+    if not nvr_dirs:
+        util.red_print("Exiting because no matched builds from all specified advisories.")
+        exit(1)
+
+    # Check build infos from Koji/Brew
+    # in order to figure out the source Git repo and commit hash for each build.
+    click.echo("Fetching build infos for {} from Koji/Brew...".format(
+        ", ".join(nvr_dirs.keys())))
+    brew_session = koji.ClientSession(constants.BREW_HUB)
+    brew_builds = tarball_sources.get_builds_from_brew(
+        brew_session, nvr_dirs.keys())
+
+    # Ready to generate tarballs
+    tarball_sources_list = []
+    for build_info in brew_builds:
+        nvr = build_info["nvr"]
+        tarball_filename = nvr + ".tar.gz"
+        click.echo("Generating tarball source {} for {}...".format(
+            tarball_filename, nvr))
+
+        with tempfile.NamedTemporaryFile(suffix="-" + tarball_filename, dir=working_dir) as temp_tarball:
+            temp_tarball_path = temp_tarball.name
+            LOGGER.debug(
+                "Temporary tarball file is {}".format(temp_tarball_path))
+
+            tarball_sources.generate_tarball_source(temp_tarball, nvr + "/", os.path.join(working_dir, "repos", build_info["name"]),
+                                                    build_info["source"])
+            for dest_dir in nvr_dirs[nvr]:
+                mkdirs(dest_dir)
+                tarball_abspath = os.path.abspath(
+                    os.path.join(dest_dir, tarball_filename))
+                if os.path.exists(tarball_abspath):
+                    util.yellow_print(
+                        "File {} will be overwritten.".format(tarball_abspath))
+
+                LOGGER.debug("Copying {} to {}...".format(
+                    temp_tarball_path, tarball_abspath))
+                shutil.copy2(temp_tarball_path, tarball_abspath)
+                tarball_sources_list.append(tarball_abspath)
+                util.green_print(
+                    "Created tarball source {}.".format(tarball_abspath))
+
+    print_success_message(tarball_sources_list, out_dir)
+
+
+def mkdirs(path):
+    """ Make sure a directory exists. Similar to shell command `mkdir -p`.
+
+    This function will not be necessary when fully migrated to Python 3.
+    """
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:  # ignore if dest_dir exists
+            raise
+
+
+def print_success_message(tarball_sources_list, out_dir):
+    relative_paths = [os.path.join(os.path.relpath(os.path.dirname(
+        path), out_dir), os.path.basename(path)) for path in tarball_sources_list]
+    relative_paths.sort()
+
+    util.green_print("""
+
+All tarball sources are successfully created.
+
+To send all tarball sources to rcm-guest, run:
+
+    rsync -avz {} ocp-build@rcm-guest.app.eng.bos.redhat.com:/mnt/rcm-guest/puddles/RHAOS/container-sources/
+
+Then notify RCM (https://projects.engineering.redhat.com/projects/RCM/issues) that the following tarball sources have been uploaded to rcm-guest:
+
+{}
+    """.format(pipes.quote(os.path.abspath(out_dir) + "/"), "\n".join(relative_paths)))
---
+++ elliottlib/tarball_sources.py
@@ -0,0 +1,164 @@
+from __future__ import unicode_literals, print_function, with_statement
+from future.builtins import str
+import logutil
+from typing import List, Set, Dict, Tuple, Text, Optional
+import os
+from io import BytesIO
+import collections
+import tarfile
+import pygit2
+import errata_tool
+import koji
+import logging
+import constants
+from future.standard_library import install_aliases
+install_aliases()
+from urllib.parse import urlparse, urldefrag
+
+# Exclude those files from the outputing tarball sources:
+TARBALL_IGNORES = {".gitignore", ".oit", "container.yaml",
+                   "content_sets.yml", "gating.yaml", "sources", "additional-tags"}
+
+LOGGER = logutil.getLogger(__name__)
+
+BuildWithProductVersion = collections.namedtuple(
+    "BuildWithProductVersion", ["nvr", "product", "product_version"])
+
+
+def find_builds_from_advisory(advisory_number, components):
+    """ Returns a filtered list of builds attached to advisories
+
+    NOTE: This function fetches the advisory info and attached builds using `errata_tool.Erratum()` Python API,
+    which is pretty SLOW because it iterates over builds for signature checking but doesn't have an option to disable.
+    Note that API cannot be called concurrently due to a race condition in its API implementation (instances of errata_tool.Erratum share class variables).
+
+    :param advisory_number: an advisory number
+    :param components: list of Koji/Brew components or NVRs to filter builds on the advisory
+    :return: list of triple in the form of `(nvr, product, product_version)`
+    """
+
+    LOGGER.debug(
+        "Fetching advisory {} from Errata Tool...".format(advisory_number))
+
+    advisory = errata_tool.Erratum(errata_id=advisory_number)
+    LOGGER.info("Got info for advisory {} - {} - {}: {} - {}".format(advisory_number, advisory.errata_state,
+                                                                     advisory.errata_name, advisory.synopsis, advisory.url()))
+    flattened_builds = [BuildWithProductVersion(nvr, advisory._product, product_version) for product_version,
+                        nvrs in advisory.errata_builds.items() for nvr in nvrs]
+
+    def matches_components(build):
+        for component in components:
+            if build[0].startswith(component):
+                return True
+        return False
+    filtered_builds = [
+        build for build in flattened_builds if matches_components(build)]
+    return filtered_builds
+
+
+def get_builds_from_brew(session, build_nvrs):
+    """Get information of multiple Koji/Brew builds
+
+    :param session: instance of :class:`koji.ClientSession`
+    :param builds_nvrs: list of build nvr strings or numbers.
+    :return: an iterator over Koji/Brew build objects
+    """
+    LOGGER.debug(
+        "Fetching build info for {} from Koji/Brew...".format(build_nvrs))
+    # Use Koji multicall interface to boost performance. See https://pagure.io/koji/pull-request/957
+    futures = {}
+    with session.multicall(strict=True):
+        for nvr in build_nvrs:
+            futures[nvr] = session.getBuild(nvr)
+    for nvr, build in futures.items():
+        LOGGER.debug("Got build info for {} from Brew.".format(nvr))
+        yield build
+
+
+def generate_tarball_source(tarball_file, prefix, local_repo_path, source_url, force_fetch=False):
+    """ Gereate a tarball source from specified commit of a remote Git repository.
+
+    This function uses pygit2 (libgit2) to walkthrough files of a commit.
+
+    :param tarball_file: File object to write the tarball into
+    :param prefix: Prepend a prefix (usually a directory) to files placed in the tarball.
+    :param local_repo_path: Clone the remote repository into this local directory.
+    :param source_url: Remote source repo url and commit hash seperated by `#`.
+    :param force_fetch: Force download objects and refs from another repository
+    """
+    source_repo_url, source_commit_hash = urldefrag(source_url)
+    assert source_commit_hash
+    LOGGER.debug("Source is from repo {}, commit {}".format(
+        source_repo_url, source_commit_hash))
+
+    git_commit = None # type: pygit2.Commit
+
+    if os.path.isdir(local_repo_path) and os.listdir(local_repo_path):
+        LOGGER.debug(
+            "Local Git repo {} exists. Examining...".format(local_repo_path))
+        discovered_repo = pygit2.discover_repository(
+            local_repo_path, False, os.path.abspath(os.path.dirname(local_repo_path)))
+        if not discovered_repo:
+            raise ValueError(
+                "{} exists but is not a valid Git repo.".format(local_repo_path))
+        repo = pygit2.Repository(discovered_repo)
+        origin = repo.remotes["origin"]  # type: pygit2.Remote
+        if origin.url != source_repo_url:
+            raise ValueError(
+                "Found a different local Git repo in {}".format(discovered_repo))
+        LOGGER.info(
+            "Use existing local Git repo {}.".format(local_repo_path))
+        fetch = force_fetch
+        if not force_fetch:
+            try:
+                git_commit = repo.revparse_single(source_commit_hash).peel(pygit2.Commit)
+            except KeyError:
+                fetch = True
+        if fetch:
+            LOGGER.info("Fetching latest objects and refs...")
+            repo.remotes["origin"].fetch()
+    else:
+        LOGGER.info("Cloning from {}...".format(source_repo_url))
+        repo = pygit2.clone_repository(source_repo_url, local_repo_path)
+
+    if not git_commit:
+        git_commit = repo.revparse_single(source_commit_hash).peel(pygit2.Commit)
+    LOGGER.info("Generating source from commit {}, author: {} <{}> message:{}".format(
+        git_commit.id, git_commit.author.name, git_commit.author.email, git_commit.message))
+
+    LOGGER.debug("Creating tarball {}...".format(tarball_file.name))
+    with tarfile.open(fileobj=tarball_file, mode="w:gz") as archive:
+        stack = [(git_commit.tree, "")]
+        while stack:
+            root, path = stack.pop()
+            for _entry in root:
+                entry = _entry  # type: pygit2.TreeEntry
+                full_name = path + entry.name
+                if full_name in TARBALL_IGNORES:
+                    LOGGER.info(
+                        "Excluded {} from source tarball.".format(full_name))
+                    continue
+                if entry.type == "tree":
+                    stack.append((repo.get(entry.id), full_name + "/"))
+                    continue
+                info = tarfile.TarInfo(prefix + full_name)
+                info.mtime = git_commit.committer.time
+                info.uname = info.gname = 'root'  # Git does this!
+                if entry.type == "commit":
+                    info.type = tarfile.DIRTYPE
+                    archive.addfile(info)
+                    LOGGER.warning("Created placeholder dir for submodule {}: {}. Dist-git repos usually don't use submodules!".format(
+                        full_name, entry.id))
+                elif entry.type == "blob":
+                    blob = repo.get(entry.id)  # type: pygit2.Blob
+                    if entry.filemode == pygit2.GIT_FILEMODE_LINK:
+                        info.type = tarfile.SYMTYPE
+                        info.linkname = blob.data.decode("utf-8")
+                        info.mode = 0o777  # symlinks get placeholder
+                        info.size = 0
+                    else:
+                        info.mode = entry.filemode
+                        info.size = blob.size
+                    archive.addfile(info, BytesIO(blob.data))
+                    LOGGER.debug("Added {}".format(full_name))
+    tarball_file.flush()  # important to write to a temp file
