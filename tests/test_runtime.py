3012d9adaa3b7cc3dfbfad222e9647a8939bb5f3
Yuxiang Zhu <yuxzhu@redhat.com>
2019-11-09 16:41:36 +0800

    Fix flake8 errors and unittests for code modified by Futurize porting stage 1

--- tests/test_runtime.py
+++ tests/test_runtime.py
@@ -6,7 +6,7 @@ from elliottlib.runtime import Runtime
 
 class RuntimeTestCase(unittest.TestCase):
     def test_parallel_exec(self):
-        ret = Runtime._parallel_exec(lambda x: x * 2, xrange(5), n_threads=20)
+        ret = Runtime._parallel_exec(lambda x: x * 2, range(5), n_threads=20)
         self.assertEqual(ret.get(), [0, 2, 4, 6, 8])
 
 
