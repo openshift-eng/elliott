d87e6c41324a64a40916b9c98cb93779bec0c52b
Joep van Delft <jdelft@redhat.com>
2020-08-28 14:09:39 +0200

    find-bugs: Add --dry-run
    ...and make search url clickable by ensuring component values are
    url-escaped

--- elliottlib/cli/__main__.py
+++ elliottlib/cli/__main__.py
@@ -110,8 +110,12 @@ pass_runtime = click.make_pass_decorator(Runtime)
 @click.option("--into-default-advisories",
               is_flag=True,
               help='attaches bugs found to their correct default advisories, e.g. operator-related bugs go to "extras" instead of the default "image", bugs filtered into "none" are not attached at all.')
+@click.option("--noop", "--dry-run",
+              is_flag=True,
+              default=False,
+              help="Don't change anything")
 @pass_runtime
-def find_bugs(runtime, advisory, default_advisory_type, mode, status, id, from_diff, flag, report, into_default_advisories):
+def find_bugs(runtime, advisory, default_advisory_type, mode, status, id, from_diff, flag, report, into_default_advisories, noop):
     """Find Red Hat Bugzilla bugs or add them to ADVISORY. Bugs can be
 "swept" into the advisory either automatically (--mode sweep), or by
 manually specifying one or more bugs using --mode list and the --id option.
@@ -229,6 +233,14 @@ advisory with the --add option.
         green_prefix("Found {} bugs:".format(len(bugs)))
     click.echo(" {}".format(", ".join([str(b.bug_id) for b in bugs])))
 
+    if len(flag) > 0:
+        for bug in bugs:
+            for f in flag:
+                if noop:
+                    click.echo(f'Would have updated bug {bug.id} by setting flag {f}')
+                    continue
+                bug.updateflags({f: "+"})
+
     if report:
         green_print("{:<8s} {:<25s} {:<12s} {:<7s} {:<10s} {:60s}".format("ID", "COMPONENT", "STATUS", "SCORE", "AGE", "SUMMARY"))
         for bug in bugs:
@@ -241,13 +253,8 @@ advisory with the --add option.
                                                                                     days_ago,
                                                                                     bug.summary[:60]))
 
-    if len(flag) > 0:
-        for bug in bugs:
-            for f in flag:
-                bug.updateflags({f: "+"})
-
     if advisory and not default_advisory_type:  # `--add ADVISORY_NUMBER` should respect the user's wish and attach all available bugs to whatever advisory is specified.
-        elliottlib.errata.add_bugs_with_retry(advisory, bugs)
+        elliottlib.errata.add_bugs_with_retry(advisory, bugs, noop=noop)
         return
 
     # If --use-default-advisory or --into-default-advisories is given, we need to determine which bugs should be swept into which advisory.
@@ -266,11 +273,11 @@ advisory with the --add option.
         impetus_bugs["image"] = {b for b in bugs if b.component not in extra_components}
 
     if default_advisory_type and impetus_bugs.get(default_advisory_type):
-        elliottlib.errata.add_bugs_with_retry(advisory, impetus_bugs[default_advisory_type])
+        elliottlib.errata.add_bugs_with_retry(advisory, impetus_bugs[default_advisory_type], noop=noop)
     elif into_default_advisories:
         for impetus, bugs in impetus_bugs.items():
             if bugs:
-                elliottlib.errata.add_bugs_with_retry(runtime.group_config.advisories[impetus], bugs)
+                elliottlib.errata.add_bugs_with_retry(runtime.group_config.advisories[impetus], bugs, noop=noop)
 
 
 #
