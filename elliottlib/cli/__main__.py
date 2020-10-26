d7caee0855cdb58bc02b8316037f7fb5eca72838
Luke Meyer <lmeyer@redhat.com>
2020-10-26 12:02:04 -0400

    [ART-1054] introduce verify-attached-operators (#161)
    Verify attached operator manifest references are shipping or already shipped.
    
    Takes a list of advisories that may contain operator metadata/bundle builds or image builds that are shipping alongside. Then determines whether the operator manifests refer only to images that have shipped in the past or are shipping in these advisories. An error is raised if there are no manifest builds attached, or if any references are missing.

--- elliottlib/cli/__main__.py
+++ elliottlib/cli/__main__.py
@@ -60,6 +60,7 @@ from elliottlib.cli.verify_attached_operators_cli import verify_attached_operato
 from elliottlib.cli.verify_attached_bugs_cli import verify_attached_bugs_cli
 from elliottlib.cli.find_cve_flaws_cli import find_cve_flaws_cli
 from elliottlib.cli.advisory_drop_cli import advisory_drop_cli
+from elliottlib.cli.verify_attached_operators_cli import verify_attached_operators_cli
 
 # 3rd party
 import bugzilla
@@ -697,6 +698,7 @@ cli.add_command(verify_attached_operators_cli)
 cli.add_command(verify_attached_bugs_cli)
 cli.add_command(find_cve_flaws_cli)
 cli.add_command(advisory_drop_cli)
+cli.add_command(verify_attached_operators_cli)
 
 
 # -----------------------------------------------------------------------------
