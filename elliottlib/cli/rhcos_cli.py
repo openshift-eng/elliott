8605dcb3a0c4f92dc1227b59619a62de0f427045
Siddharth Sharma <sidsharm@redhat.com>
2021-08-04 15:50:08 -0400

    various improvements

--- elliottlib/cli/rhcos_cli.py
+++ elliottlib/cli/rhcos_cli.py
@@ -1,34 +1,37 @@
-import click
+import click, re
 from elliottlib.cli.common import cli
 from elliottlib import rhcos, cincinnati, util
 
 
 @cli.command("rhcos", short_help="Show details about RHCOS packages")
-@click.option('--from-spec', '-s', 'pullspec',
-              help='Show details of RHCOS in the given payload pullspec')
 @click.option('--latest', '-l', 'latest',
               is_flag=True,
               help='Show details of latest RHCOS builds')
 @click.option('--latest-ocp', '-o', 'latest_ocp',
               is_flag=True,
               help='Show details of RHCOS in latest public OCP release for a given version')
-@click.option('--packages', '-p', 'packages',
-              help='Show details only these packages. Comma separated package names')
+@click.option('--release', '-r', 'release',
+              help='Show details for this OCP release. Can be a full pullspec or a named release ex: 4.8.4')
 @click.option('--arch', 'arch',
               type=click.Choice(util.brew_arches + ['all']),
               help='Specify architecture. Default is x86_64. "all" to get all arches. aarch64 only works for 4.8+')
+@click.option('--packages', '-p', 'packages',
+              help='Show details only these packages. Comma separated package names')
 @click.option('--go', '-g', 'go',
               is_flag=True,
               help='Show go version for packages that are go binaries')
 @click.pass_obj
-def rhcos_cli(runtime, pullspec, latest, latest_ocp, packages, arch, go):
+def rhcos_cli(runtime, latest, latest_ocp, release, packages, arch, go):
     """
     Show details about rhcos packages
 
     Usage:
 
 \b
-    $ elliott --group openshift-4.6 rhcos -s quay.io/openshift-release-dev/ocp-release:4.6.31-x86_64
+    $ elliott rhcos -r registry.ci.openshift.org/ocp-s390x/release-s390x:4.8.0-0.nightly-s390x-2021-07-31-070046
+
+\b
+    $ elliott rhcos -r 4.6.31 -p runc --go --arch all
 
 \b
     $ elliott --group openshift-4.8 rhcos -l -p runc
@@ -39,31 +42,40 @@ def rhcos_cli(runtime, pullspec, latest, latest_ocp, packages, arch, go):
 \b
     $ elliott --group openshift-4.8 rhcos -o -p skopeo,podman --arch all
 """
-    count_options = sum(map(bool, [pullspec, latest, latest_ocp]))
+    count_options = sum(map(bool, [release, latest, latest_ocp]))
     if count_options > 1:
         raise click.BadParameter("Use only one of --from-spec, --latest, --latest-ocp")
 
-    if arch and not (latest or latest_ocp):
-        raise click.BadParameter("--arch can only be used with --latest, --latest-ocp")
+    if arch and ('/' in release):
+        raise click.BadParameter("--arch=all cannot be used with --release <pullspec>")
 
-    runtime.initialize()
-    major = runtime.group_config.vars.MAJOR
-    minor = runtime.group_config.vars.MINOR
-    version = f'{major}.{minor}'
+    if latest or latest_ocp:
+        runtime.initialize()
+        major = runtime.group_config.vars.MAJOR
+        minor = runtime.group_config.vars.MINOR
+        version = f'{major}.{minor}'
+    else:
+        version = re.search(r'(\d+\.\d+).', release).groups()[0]
+
     arch = 'x86_64' if not arch else arch
 
     if arch == 'all':
         for a in util.brew_arches:
-            _rhcos(version, pullspec, latest, latest_ocp, packages, a, go)
+            _rhcos(version, release, latest, latest_ocp, packages, a, go)
     else:
-        _rhcos(version, pullspec, latest, latest_ocp, packages, arch, go)
+        _rhcos(version, release, latest, latest_ocp, packages, arch, go)
 
 
-def _rhcos(version, pullspec, latest, latest_ocp, packages, arch, go):
-    if arch == 'aarch64' and version < '4.8':
+def get_pullspec(release, arch):
+    return f'quay.io/openshift-release-dev/ocp-release:{release}-{arch}'
+
+
+def _rhcos(version, release, latest, latest_ocp, packages, arch, go):
+    if arch == 'aarch64' and version < '4.9':
         return
 
     build_id = ''
+    pullspec = ''
     if latest or latest_ocp:
         if latest:
             print(f'Looking up latest rhcos build id for {version} {arch}')
@@ -74,8 +86,14 @@ def _rhcos(version, pullspec, latest, latest_ocp, packages, arch, go):
             release = cincinnati.get_latest_candidate_ocp(version, arch)
             if not release:
                 return
-            print(f'OCP release found: {release}')
-            pullspec = f'quay.io/openshift-release-dev/ocp-release:{release}-{arch}'
+
+    if release:
+        if '/' in release:
+            pullspec = release
+        else:
+            print(f'OCP release: {release}-{arch}')
+            pullspec = get_pullspec(release, arch)
+
 
     if pullspec:
         print(f"Looking up rhcos build id for {pullspec}")
@@ -84,6 +102,8 @@ def _rhcos(version, pullspec, latest, latest_ocp, packages, arch, go):
 
     if build_id:
         nvrs = rhcos.get_rpm_nvrs(build_id, version, arch)
+        if not nvrs:
+            return
         if packages:
             packages = [p.strip() for p in packages.split(',')]
             nvrs = [p for p in nvrs if p[0] in packages]
