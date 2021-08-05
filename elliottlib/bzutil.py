267b4b8c1f1923d1e2bb7583cc27230b3bacfa02
ximhan <ximhan@redhat.com>
2021-08-05 15:19:48 +0800

    add comment when move bug to QA

--- elliottlib/bzutil.py
+++ elliottlib/bzutil.py
@@ -11,9 +11,11 @@ from typing import Iterable, List
 import urllib.parse
 import xmlrpc.client
 from time import sleep
+import calendar
 
 import bugzilla
 import click
+from github import Github
 from koji import ClientSession
 
 from elliottlib import constants, exceptions, logutil
@@ -175,6 +177,32 @@ def set_state(bug, desired_state, noop=False):
     bug.setstatus(status=desired_state,
                   comment=comment,
                   private=True)
+
+
+def add_release_comment(bug, major_version, minor_version, noop=False):
+    """Give a rough idea of when a BZ is going out
+
+    :param bug
+    :param major_version: Major release version, 4
+    :param minor_version: Minor release version, 6
+    :param noop: Do not do anything
+    """
+    if noop:
+        logger.info(f"add target planed releae version and release date in comment")
+        return
+    gitclinet = Github()
+    repo = gitclient.get_repo("openshift/cincinnati-graph-data")
+    content = repo.get_contents("channels/fast-{}.{}.yaml".format(major_version, minor_version), ref="master")
+    fast_channel = yaml.load(content.decoded_content, Loader=yaml.FullLoader) # get latest release in fast channel
+    next_release = int(version['versions'][-1].split('.')[-1]) + 1
+    next_release_version = "{}.{}.{}".format(major_version, minor_version, next_release)
+    today = datetime.date.today()
+    while today.weekday() != calendar.MONDAY:
+        today += datetime.timedelta(days = 1)
+    today += datetime.timedelta(days = 7) #next next monday
+    next_release_date = today.sfttime("%Y-%m-%d")
+    comment = f"This bug will be shipped in {next_release_version} which plan to release at around {next_release_date}."
+    bug.addcomment(comment=comment, private=True)
 
 
 def create_placeholder(bz_data, kind):
