7b08af00da1a399a9b7acb57d1e306b854256de2
Siddharth Sharma <sidsharm@redhat.com>
2022-06-17 16:00:41 -0400

    4.6 below is not compatible

--- functional_tests/test_find_bugs_qe.py
+++ functional_tests/test_find_bugs_qe.py
@@ -8,12 +8,10 @@ class FindBugsQETestCase(unittest.TestCase):
         out = subprocess.check_output(
             constants.ELLIOTT_CMD
             + [
-                "--assembly=stream", "--group=openshift-4.3", "find-bugs:qe", '--noop'
+                "--assembly=stream", "--group=openshift-4.6", "find-bugs:qe", '--noop'
             ]
         )
-        search_string = "Searching for bugs with status MODIFIED and target release(s): 4.3.z, 4.3.0"
         result = out.decode("utf-8")
-        self.assertIn(search_string, result)
         self.assertRegex(self, result, "Found \\d+ bugs")
 
 
