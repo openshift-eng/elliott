32152320d263b9c2a70bd4756ae8858f36f2c9a1
Luke Meyer <lmeyer@redhat.com>
2022-08-24 15:53:39 -0400

    [ART-4315] rhcos: release-browser moved to new cluster

--- elliottlib/rhcos.py
+++ elliottlib/rhcos.py
@@ -47,7 +47,7 @@ def _build_meta(build_id, version, arch="x86_64", private=False, meta_type="meta
     meta_type is "meta" for the build record or "commitmeta" for its ostree content.
 
     @return  a "meta" build record e.g.:
-     https://releases-rhcos-art.cloud.privileged.psi.redhat.com/storage/releases/rhcos-4.1/410.81.20200520.0/meta.json
+     https://releases-rhcos-art.apps.ocp-virt.prod.psi.redhat.com/storage/releases/rhcos-4.1/410.81.20200520.0/meta.json
      {
          "buildid": "410.81.20200520.0",
          ...
