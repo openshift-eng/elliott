ad9a298d500b0e5d0383f280bf723b6e4dda5ddd
ximhan <ximhan@redhat.com>
2022-09-14 19:52:26 +0800

    use errata_tool lib

--- elliottlib/bzutil.py
+++ elliottlib/bzutil.py
@@ -606,8 +606,11 @@ class JIRABugTracker(BugTracker):
         if noop:
             print(f"Would've removed bugs: {bugids}")
             return
-        advisory_id = advisory_obj.errata_id
-        return errata.remove_multi_jira_issues(advisory_id, bugids)
+        try:
+            advisory_obj.removeJIRAIssues(jira_list)
+            advisory_obj.commit()
+        except ErrataException as e:
+            raise exceptions.ElliottFatalError(getattr(e, 'message', repr(e)))
 
     def attach_bugs(self, advisory_id: int, bugids: List, noop=False, verbose=False):
         return errata.add_jira_bugs_with_retry(advisory_id, bugids, noop=noop)
