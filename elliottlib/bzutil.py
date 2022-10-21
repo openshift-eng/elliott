ea14a47e44b91b3d743becdc70d8ccbce9b88feb
ximhan <ximhan@redhat.com>
2022-10-21 19:00:55 +0800

    optimize output

--- elliottlib/bzutil.py
+++ elliottlib/bzutil.py
@@ -61,7 +61,7 @@ class Bug:
         return set(constants.TRACKER_BUG_KEYWORDS).issubset(set(self.keywords))
 
     def is_cve_in_summary(self):
-        return True if re.search(r'^CVE-\d+-\d+', self.summary) else False
+        return bool(re.search(r'^CVE-\d+-\d+', self.summary))
 
     def is_flaw_bug(self):
         return self.product == "Security Response" and self.component == "vulnerability"
