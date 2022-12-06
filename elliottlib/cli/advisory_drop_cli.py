92dbf499da8bdd68803d355aa9c5ca27ee1cd672
Ximin han <ximhan@redhat.com>
2022-12-06 18:47:31 +0800

    fix-drop-advisory

--- elliottlib/cli/advisory_drop_cli.py
+++ elliottlib/cli/advisory_drop_cli.py
@@ -24,13 +24,16 @@ def advisory_drop_cli(advisory):
         click.echo(f'Advisory {advisory} is already dropped')
         return
 
+    # move advisory status to NEW_FILES
+    if adv.errata_state != "NEW_FILES":
+        adv.setState("NEW_FILES")
+        adv.commit()
+
     # Remove bugs and builds before dropping the advisory
     all_bugs = adv.errata_bugs
     all_jiras = adv.jira_issues
     all_builds = list(set(itertools.chain.from_iterable(adv.errata_builds.values())))
     if all_bugs or all_jiras or all_builds:
-        if adv.errata_state != "NEW_FILES":
-            adv.setState("NEW_FILES")
         if all_bugs:
             adv.removeBugs(all_bugs)
         if all_jiras:
@@ -44,7 +47,8 @@ def advisory_drop_cli(advisory):
     data = 'utf8=%E2%9C%93&reason=Dropping+unused+advisory%21&commit=Dropping+unused+advisory'
     headers = {"Content-Type": "text/plain"}
     r = requests.post(url, auth=HTTPSPNEGOAuth(), data=data, headers=headers)
-    if not r.ok:
+    adv.refresh()
+    if adv.errata_state != "DROPPED_NO_SHIP":
         raise ElliottFatalError(f'Failed to drop advisory {advisory}. Got status code {r.status_code}. Are you the owner of advisory {advisory}?')
 
     click.echo(f'Successfully dropped advisory {advisory}')
