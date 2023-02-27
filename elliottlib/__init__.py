e158e828f08470c69963efd0ec85daae4f0e72ca
Yuxiang Zhu <yuxzhu@redhat.com>
2023-02-27 18:35:24 +0800

    Fix version detection error on pip installations
    Fix the following error when invoking `elliott --version` on an
    installation without the source git repository:
    
    ```
    LookupError: setuptools-scm was unable to detect version for /usr/local/lib/python3.11/site-packages.
    ```
    
    Steps to reproduce:
    
    ```sh
    pip install .
    
    elliott --version
    ```
    
    This can be solved by introducting setuptools_scm to write a file
    containing the to-be-installed version.

--- elliottlib/__init__.py
+++ elliottlib/__init__.py
@@ -1,15 +1,16 @@
 import os
 import sys
-from setuptools_scm import get_version
 
 if sys.version_info < (3, 8):
     sys.exit('Sorry, Python < 3.8 is no longer supported.')
-from .runtime import Runtime
+
+from elliottlib.runtime import Runtime
 
 
 def version():
-    return get_version(
-        root=os.path.abspath(
-            os.path.join(os.path.dirname(__file__), '..')
-        )
-    )
+    try:
+        from ._version import version
+    except ImportError:
+        from setuptools_scm import get_version
+        version = get_version()
+    return version
