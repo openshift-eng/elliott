dc74934607b5b104cdd843ba9e43d1544090058b
Siddharth Sharma <sidsharm@redhat.com>
2023-03-01 17:14:48 -0500

    improvements

--- elliottlib/cli/find_bugs_wo_target_version.py
+++ elliottlib/cli/find_bugs_wo_target_version.py
@@ -2,15 +2,13 @@ import json
 import click
 from elliottlib import (Runtime, logutil)
 from elliottlib.cli import common
-from elliottlib.cli.common import click_coroutine
-
 from elliottlib.cli.find_bugs_sweep_cli import FindBugsSweep, print_report
-
 
 logger = logutil.getLogger(__name__)
 
 
-@common.cli.command("find-bugs:notv", short_help="Find qualified bugs (sweep) that do not have a target version set")
+@common.cli.command("find-bugs:notv", short_help="Find qualified ART managed jira bugs (sweep) that do not have a "
+                                                 "target version field set")
 @click.option("--comment", "comment",
               required=False,
               help="Add comment to found bugs")
@@ -19,8 +17,21 @@ logger = logutil.getLogger(__name__)
               is_flag=True,
               help="Output a detailed report of found bugs")
 @click.pass_obj
-@click_coroutine
+@common.click_coroutine
 async def find_bugs_no_tv(runtime: Runtime, comment, report):
+    """Find qualified ART managed jira bugs (sweep) that do not have a target version field set
+    The group only determines which components are filtered out of the search
+    (https://github.com/openshift/ocp-build-data/blob/openshift-4.12/bug.yml)
+    Bugs are not restricted to a specific version.
+    This is only intended to be used with --assembly=stream
+
+    $ elliott -g openshift-4.12 find-bugs:notv --report
+
+    $ elliott -g openshift-4.12 find-bugs:notv --comment "This bug has been found to have 'Target Version' field set
+    to empty. ART automation strictly relies on it to attach bugs to advisories. Please set the Target Version to the
+    release that you expect the fix to go in."
+
+    """
     if runtime.assembly != 'stream':
         raise click.BadParameter("This command is intended to work only with --assembly=stream",
                                  param_hint='--assembly')
@@ -31,14 +42,13 @@ async def find_bugs_no_tv(runtime: Runtime, comment, report):
     bugs = find_bugs_obj.search(bug_tracker_obj=bug_tracker, verbose=runtime.debug,
                                 with_target_release=False, custom_query=' AND "Target Version" is EMPTY AND '
                                                                         'component != "Release"')
-    print(f'Found {len(bugs)} bugs with status={find_bugs_obj.status} and no Target Version set')
-    print([b.id for b in bugs])
-    # comment = "This bug has been found to have no 'Target Version' field set.
-    # ART automation strictly relies on it to attach bugs to advisories.
-    # Please set the target version to the release that you expect the fix to go in."
+    click.echo(f'Found {len(bugs)} bugs with status={find_bugs_obj.status} and no Target Version set')
     if report:
         print_report(bugs)
+    else:
+        click.echo([b.id for b in bugs])
 
     if comment:
+        logger.info('Adding comment to bugs...')
         for bug in bugs:
             bug_tracker.add_comment(bug.id, comment, private=True)
